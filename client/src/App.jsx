6f806115ebc6c7153eb2a1caf0b84f195d5941ed
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2023-10-30 09:44:49 -0300

    Base components and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Home from './components/Home';
+import Cards from './components/Cards';
+import Card from './components/Card';
+import Detail from './components/Detail';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/cards' element={<Cards />} />
+        <Route path='/card' element={<Card />} />
+        <Route path='/detail' element={<Detail />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
